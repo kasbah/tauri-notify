bbd0d61842d13bbd2f05187161aa1da764841e3b
Kaspar Emanuel <kaspar@kitspace.org>
2023-03-23 15:56:25 +0000

    Test out filesystem notify

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,31 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
-fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+use notify::{RecursiveMode, Result, Watcher};
+use std::path::Path;
+
+fn main() -> Result<()> {
+    println!("hello!");
+    let mut watcher = notify::recommended_watcher(|res| match res {
+        Ok(event) => println!("event: {:?}", event),
+        Err(e) => println!("watch error: {:?}", e),
+    })?;
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher.watch(Path::new("."), RecursiveMode::Recursive)?;
+
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+
+    Ok(())
 }
 
 #[tauri::command]
 fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
+    format!("Hello, {}!", name)
 }
